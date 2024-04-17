af36526792af132b2af1e60807fce718ec51d75e
oussama <oussema.lamine20@gmail.com>
2024-04-17 16:11:46 +0100

    updating the user page

--- src/views/profile/UserProfile.js
+++ src/views/profile/UserProfile.js
@@ -37,7 +37,14 @@ const UserProfile = () => {
             <CCardBody>
               <CTable>
                 <CTableBody>
-
+                <CAlert color="primary">A simple primary alert—check it out!</CAlert>
+              <CAlert color="secondary">A simple secondary alert—check it out!</CAlert>
+              <CAlert color="success">A simple success alert—check it out!</CAlert>
+              <CAlert color="danger">A simple danger alert—check it out!</CAlert>
+              <CAlert color="warning">A simple warning alert—check it out!</CAlert>
+              <CAlert color="info">A simple info alert—check it out!</CAlert>
+              <CAlert color="light">A simple light alert—check it out!</CAlert>
+              <CAlert color="dark">A simple dark alert—check it out!</CAlert>
                 </CTableBody>
               </CTable>
             </CCardBody>
